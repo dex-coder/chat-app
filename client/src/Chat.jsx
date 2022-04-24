770e936c81d6732c1f2262de640892f52d1a2895
Dex Coder <60599437+dex-coder@users.noreply.github.com>
2022-04-24 23:51:52 +0500

    final commit

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([])
 
   //sending msg to the backend and make object of message
   const sendMessage = async () => {
-      console.log("hello");
     if (currentMessage !== "") {
       const messageData = {
         room: room,
@@ -17,28 +18,56 @@ const Chat = ({ socket, username, room }) => {
           new Date(Date.now()).getMinutes(),
       };
 
-      await socket.emit("send_message", messageData)
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
   useEffect(() => {
-      socket.on("receive_message", (data) => {
-          console.log(data);
-      })
-  }, [socket])
+    socket.off("receive_message").on("receive_message", (data) => {
+      console.log(data);
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
   return (
     <div className="chat-window">
       <div className="chat-header">
         <p>Live chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+      <ScrollToBottom className="message-container">
+      {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+                //key={messageContent.time}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          </ScrollToBottom>
+      </div>
       <div className="chat-footer">
         <input
           type="text"
           placeholder="Hey..."
+          value={currentMessage}
           onChange={(e) => setCurrentMessage(e.target.value)}
-        />{" "}
+          //checking if the key press is equal to enter then we call the function
+          onKeyPress={(event) => { event.key === "Enter" && sendMessage() }}
+        />
         <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -27,9 +27,9 @@ io.on("connection", (socket) => {
 
     //receving msg here
     socket.on("send_message", (data) => {
-        console.log(data);
         socket.to(data.room).emit("receive_message", data);
-    })
+      });
+    
 
     socket.on("diconnected", () => {
         console.log("User Disconnected", socket.id);
